eb10472a5fee26c830fb6d37ff5c74661e88fdbd
Hawkynt <github@synthelicz.de>
2019-06-03 19:39:21 +0200

    + DataGridViewRowHeightAttribute

--- System.Windows.Forms.DataGridViewExtensions.cs
+++ System.Windows.Forms.DataGridViewExtensions.cs
@@ -477,6 +477,79 @@ namespace System.Windows.Forms {
   }
 
   /// <summary>
+  /// Allows setting an exact height in pixels for automatically generated columns.
+  /// </summary>
+  [AttributeUsage(AttributeTargets.Struct | AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+  internal sealed class DataGridViewRowHeightAttribute : Attribute {
+    public string RowHeightProperty { get; }
+    public string CustomRowHeightEnabledProperty { get; }
+    public string CustomRowHeightProperty { get; }
+    public int HeightInPixel { get; }
+
+    private readonly Action<DataGridViewRow, object> _applyRowHeightAction;
+
+    public DataGridViewRowHeightAttribute(int heightInPixel) {
+      this.HeightInPixel = heightInPixel;
+
+      this._applyRowHeightAction = this._ApplyPixelRowHeightUnconditional;
+    }
+
+    public DataGridViewRowHeightAttribute(int heightInPixel, string customRowHeightEnabledProperty) {
+      this.HeightInPixel = heightInPixel;
+      this.CustomRowHeightEnabledProperty = customRowHeightEnabledProperty;
+
+      this._applyRowHeightAction = this._ApplyPixelRowHeightConditional;
+    }
+
+    public DataGridViewRowHeightAttribute(string customRowHeightProperty) {
+      this.CustomRowHeightProperty = customRowHeightProperty;
+
+      this._applyRowHeightAction = this._ApplyPropertyConrolledRowHeightUnconditional;
+    }
+
+    public DataGridViewRowHeightAttribute(string customRowHeightProperty, string customRowHeightEnabledProperty) {
+      this.CustomRowHeightProperty = customRowHeightProperty;
+      this.CustomRowHeightEnabledProperty = customRowHeightEnabledProperty;
+
+      this._applyRowHeightAction = this._ApplyPropertyConrolledRowHeightConditional;
+    }
+
+    private void _ApplyPixelRowHeightUnconditional(DataGridViewRow row, object rowData) {
+      row.MinimumHeight = this.HeightInPixel;
+      row.Height = this.HeightInPixel;
+    }
+
+    private void _ApplyPixelRowHeightConditional(DataGridViewRow row, object rowData) {
+      if (!DataGridViewExtensions.GetPropertyValueOrDefault(rowData, this.CustomRowHeightEnabledProperty, false, false, false, false))
+        return;
+
+      row.MinimumHeight = this.HeightInPixel;
+      row.Height = this.HeightInPixel;
+    }
+
+    private void _ApplyPropertyConrolledRowHeightUnconditional(DataGridViewRow row, object rowData) {
+      var originalHeight = row.Height;
+      var rowHeight = DataGridViewExtensions.GetPropertyValueOrDefault(rowData, this.CustomRowHeightProperty, originalHeight, originalHeight, originalHeight, originalHeight);
+
+      row.MinimumHeight = rowHeight;
+      row.Height = rowHeight;
+    }
+
+    private void _ApplyPropertyConrolledRowHeightConditional(DataGridViewRow row, object rowData) {
+      if (!DataGridViewExtensions.GetPropertyValueOrDefault(rowData, this.CustomRowHeightEnabledProperty, false, false, false, false))
+        return;
+
+      var originalHeight = row.Height;
+      var rowHeight = DataGridViewExtensions.GetPropertyValueOrDefault(rowData, this.CustomRowHeightProperty, originalHeight, originalHeight, originalHeight, originalHeight);
+
+      row.MinimumHeight = rowHeight;
+      row.Height = rowHeight;
+    }
+
+    public void ApplyTo(object rowData, DataGridViewRow row) => this._applyRowHeightAction?.Invoke(row, rowData);
+  }
+
+  /// <summary>
   /// Allows adjusting the cell style in a DataGridView for automatically generated columns.
   /// </summary>
   [AttributeUsage(AttributeTargets.Property, AllowMultiple = true, Inherited = true)]
@@ -967,6 +1040,7 @@ namespace System.Windows.Forms {
         _FixDisabledButtonCellStyle(type, column, cell, value);
       }
 
+      _QueryPropertyAttribute<DataGridViewRowHeightAttribute>(type).FirstOrDefault()?.ApplyTo(value, row);
     }
 
     /// <summary>
