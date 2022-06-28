3891eba6c0563e7bd5b3d9869cfed79c25d3f4f3
Hawkynt <github@synthelicz.de>
2022-06-28 11:41:32 +0200

    + syntax highlighting

--- System.Windows.Forms.RichTextBoxExtensions.cs
+++ System.Windows.Forms.RichTextBoxExtensions.cs
@@ -18,10 +18,15 @@
     If not, see <http://www.gnu.org/licenses/>.
 */
 #endregion
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 using DrawingSize = System.Drawing.Size;
 
 // ReSharper disable PartialTypeWithSinglePart
@@ -31,6 +36,7 @@ namespace System.Windows.Forms {
   internal static partial class RichTextBoxExtensions {
 
     #region Native Methods
+
     private static partial class NativeMethods {
       // ReSharper disable MemberCanBePrivate.Local
 
@@ -108,7 +114,7 @@ namespace System.Windows.Forms {
     public static void AppendTextAndScroll(this RichTextBox @this, string text, Color? color = null) {
       Contract.Requires(@this != null);
 
-      if (text.IsNullOrEmpty())
+      if (string.IsNullOrEmpty(text))
         return;
 
       AppendText(@this, text, color);
@@ -211,6 +217,7 @@ namespace System.Windows.Forms {
     #region Struct_Const_4_DrawToBitmap
 
     private const double _INCH = 14.4;
+
     #endregion
 
     public static Bitmap DrawToBitmap(this RichTextBox @this, int width, int height) {
@@ -289,5 +296,220 @@ namespace System.Windows.Forms {
       @this.SelectionLength = 0;
     }
 
+    #region richtextbox syntax highlighting
+
+    private static readonly ConditionalWeakTable<RichTextBox, SyntaxHighlighter> _syntaxHighlighterCache = new ConditionalWeakTable<RichTextBox, SyntaxHighlighter>();
+
+    public static void ApplySyntaxHighlighting(this RichTextBox @this, SyntaxHighlightingConfiguration configuration, bool reapply = false) {
+      if (@this == null)
+        throw new ArgumentNullException(nameof(@this));
+
+      if (configuration.Patterns == null || configuration.Patterns.Length <= 0)
+        return;
+
+      var highlighter = new SyntaxHighlighter(configuration);
+
+      if (reapply) {
+        if (_syntaxHighlighterCache.TryGetValue(@this, out _))
+          _syntaxHighlighterCache.Remove(@this);
+        else {
+          @this.TextChanged += Rtb_OnTextChanged;
+          @this.Disposed += Rtb_OnDispose;
+        }
+
+        _syntaxHighlighterCache.Add(@this, highlighter);
+      }
+
+      highlighter.ApplySyntaxHighlighting(@this);
+    }
+
+    public static void RemoveSyntaxHighlighting(this RichTextBox @this) {
+      if (@this == null)
+        throw new ArgumentNullException(nameof(@this));
+
+      @this.TextChanged -= Rtb_OnTextChanged;
+      @this.Disposed -= Rtb_OnDispose;
+
+      var style = new _SyntaxStyle(@this.ForeColor, @this.BackColor, @this.SelectionFont.Style);
+      style.ApplyTo(@this, 0, @this.TextLength);
+
+      if (_syntaxHighlighterCache.TryGetValue(@this, out _))
+        _syntaxHighlighterCache.Remove(@this);
+    }
+
+    private static void Rtb_OnDispose(object sender, EventArgs _) {
+      if (sender is RichTextBox rtb)
+        RemoveSyntaxHighlighting(rtb);
+    }
+
+    private static void Rtb_OnTextChanged(object sender, EventArgs e) {
+      if (!(sender is RichTextBox rtb))
+        return;
+
+      if (_syntaxHighlighterCache.TryGetValue(rtb, out var highlighter))
+        highlighter.ApplySyntaxHighlighting(rtb);
+    }
+
+    #region Syntax hightlighting classes
+
+    public interface ISyntaxStyle {
+      Color? Foreground { get; }
+      FontStyle? FontStyle { get; }
+      Color? Background { get; }
+    }
+
+    public static class SyntaxStyle {
+      public static ISyntaxStyle Foreground(Color color) => new _SyntaxStyle(color);
+      public static ISyntaxStyle Background(Color color) => new _SyntaxStyle(background: color);
+      public static ISyntaxStyle Color(Color foreground, Color background) => new _SyntaxStyle(foreground, background);
+      public static ISyntaxStyle Style(FontStyle style) => new _SyntaxStyle(fontStyle: style);
+      public static ISyntaxStyle Bold(Color? foreground = null) => new _SyntaxStyle(foreground, fontStyle: FontStyle.Bold);
+      public static ISyntaxStyle Italic(Color? foreground = null) => new _SyntaxStyle(foreground, fontStyle: FontStyle.Italic);
+      public static ISyntaxStyle Underline(Color? foreground = null) => new _SyntaxStyle(foreground, fontStyle: FontStyle.Underline);
+      public static ISyntaxStyle CreateFrom(Color? foreground = null, Color? background = null, FontStyle? fontStyle = null) => new _SyntaxStyle(foreground, background, fontStyle);
+
+    }
+
+    private struct _SyntaxStyle : ISyntaxStyle {
+
+      //TODO: maybe font cache (somehow) || maybe change FontStyle to Font Object
+
+      public Color? Foreground { get; }
+      public FontStyle? FontStyle { get; }
+      public Color? Background { get; }
+
+      public _SyntaxStyle(Color? foreground = null, Color? background = null, FontStyle? fontStyle = null) {
+        this.Foreground = foreground;
+        this.Background = background;
+        this.FontStyle = fontStyle;
+      }
+
+      public void ApplyTo(RichTextBox rtb, int startIndex, int length) {
+        rtb.SelectionStart = startIndex;
+        rtb.SelectionLength = length;
+
+        if (this.Foreground != null)
+          rtb.SelectionColor = this.Foreground.Value;
+
+        if (this.Background != null)
+          rtb.SelectionBackColor = this.Background.Value;
+
+        if (this.FontStyle != null)
+          using (var font = new Font(rtb.Font, this.FontStyle.Value)) {
+            rtb.SelectionFont = font;
+          }
+      }
+    }
+
+    public struct SyntaxHighlightingConfiguration {
+      public ISyntaxHighlightPattern[] Patterns { get; }
+
+      public SyntaxHighlightingConfiguration(IEnumerable<ISyntaxHighlightPattern> patterns) {
+        if (patterns == null)
+          throw new ArgumentNullException(nameof(patterns));
+
+        this.Patterns = patterns.ToArray();
+      }
+
+      public SyntaxHighlightingConfiguration(params ISyntaxHighlightPattern[] patterns) => this.Patterns = patterns;
+    }
+
+    public interface ISyntaxHighlightPattern {
+      Regex RegularExpression { get; }
+      ISyntaxStyle Style { get; }
+      IReadOnlyDictionary<string, ISyntaxStyle> GroupStyles { get; }
+    }
+
+    public static class SyntaxHighlightPattern {
+      public static ISyntaxHighlightPattern FromKeywords(IEnumerable<string> keywords, ISyntaxStyle style, bool ignoreCase = false) => new Pattern(new Regex($@"(?<=^|\W)({string.Join("|", keywords.Select(Regex.Escape))})(?=\W|$)", RegexOptions.Compiled | RegexOptions.Singleline | (ignoreCase ? RegexOptions.IgnoreCase : 0)), style);
+      public static ISyntaxHighlightPattern FromKeyword(string word, ISyntaxStyle style, bool ignoreCase = false) => new Pattern(new Regex($@"(?<=^|\W)({Regex.Escape(word)})(?=\W|$)", RegexOptions.Compiled | RegexOptions.Singleline | (ignoreCase ? RegexOptions.IgnoreCase : 0)), style);
+      public static ISyntaxHighlightPattern FromRegex(Regex regex, ISyntaxStyle style) => new Pattern(regex, style);
+      public static ISyntaxHighlightPattern FromRegex(Regex regex, IDictionary<string, ISyntaxStyle> styles) => new Pattern(regex, null, new ReadOnlyDictionary<string, ISyntaxStyle>(styles));
+      public static ISyntaxHighlightPattern FromPattern(string pattern, ISyntaxStyle style) => new Pattern(new Regex(pattern, RegexOptions.Compiled), style);
+      public static ISyntaxHighlightPattern FromPattern(string pattern, IDictionary<string, ISyntaxStyle> styles) => new Pattern(new Regex(pattern, RegexOptions.Compiled), null, new ReadOnlyDictionary<string, ISyntaxStyle>(styles));
+      public static ISyntaxHighlightPattern FromPattern(string pattern, RegexOptions options, ISyntaxStyle style) => new Pattern(new Regex(pattern, options | RegexOptions.Compiled), style);
+      public static ISyntaxHighlightPattern FromPattern(string pattern, RegexOptions options, IDictionary<string, ISyntaxStyle> styles) => new Pattern(new Regex(pattern, options | RegexOptions.Compiled), null, new ReadOnlyDictionary<string, ISyntaxStyle>(styles));
+
+      private struct Pattern : ISyntaxHighlightPattern {
+        public Pattern(Regex regularExpression, ISyntaxStyle style, IReadOnlyDictionary<string, ISyntaxStyle> groupStyles = null) {
+          this.RegularExpression = regularExpression;
+          this.Style = style;
+          this.GroupStyles = groupStyles;
+        }
+
+        #region Implementation of ISyntaxHighlightPattern
+
+        public Regex RegularExpression { get; }
+        public ISyntaxStyle Style { get; }
+        public IReadOnlyDictionary<string, ISyntaxStyle> GroupStyles { get; }
+
+        #endregion
+      }
+
+    }
+
+    private class SyntaxHighlighter {
+      private struct RtbState {
+        private readonly RichTextBox _owner;
+        private readonly int _selectionStart;
+        private readonly int _selectionLength;
+        private RtbState(RichTextBox owner) {
+          this._owner = owner;
+          this._selectionStart = owner.SelectionStart;
+          this._selectionLength = owner.SelectionLength;
+        }
+
+        public static RtbState Save(RichTextBox owner) => new RtbState(owner);
+
+        public void Load() {
+          var rtb = this._owner;
+          rtb.SelectionStart = this._selectionStart;
+          rtb.SelectionLength = this._selectionLength;
+        }
+
+      }
+
+      private readonly SyntaxHighlightingConfiguration _configuration;
+
+      public SyntaxHighlighter(SyntaxHighlightingConfiguration configuration) => this._configuration = configuration;
+
+      public void ApplySyntaxHighlighting(RichTextBox rtb) {
+        rtb.SuspendLayout();
+        try {
+          var rtbState = RtbState.Save(rtb);
+          try {
+            this._StyleRichTextBox(rtb);
+          } finally {
+            rtbState.Load();
+          }
+        } finally {
+          rtb.ResumeLayout(true);
+        }
+
+      }
+
+      private void _StyleRichTextBox(RichTextBox rtb) {
+        var text = rtb.Text ?? rtb.Rtf;
+        foreach (var pattern in this._configuration.Patterns)
+          foreach (Match match in pattern.RegularExpression.Matches(text)) {
+            ((_SyntaxStyle?)pattern.Style)?.ApplyTo(rtb, match.Index, match.Length);
+
+            if (pattern.GroupStyles == null)
+              continue;
+
+            foreach (var kvp in pattern.GroupStyles) {
+              var group = match.Groups[kvp.Key];
+              if (group == null || !group.Success)
+                continue;
+
+              ((_SyntaxStyle?)kvp.Value)?.ApplyTo(rtb, group.Index, group.Length);
+            }
+          }
+      }
+
+      #endregion
+
+      #endregion
+    }
   }
 }
