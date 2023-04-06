2e72fa30e72e21a5f602c28c63ba88a7f687b443
Hawkynt <github@synthelicz.de>
2023-04-06 15:46:35 +0200

    + Guard API

--- Corlib.Extensions/Guard/Against.cs
+++ Corlib.Extensions/Guard/Against.cs
@@ -1,10 +1,12 @@
 ﻿namespace Guard;
 
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
-internal static class Against {
+internal static partial class Against {
   [DebuggerHidden]
 #if SUPPORTS_INLINING
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -21,6 +23,126 @@ internal static class Against {
   public static void ArgumentIsNull<T>([NotNull] T value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) where T : class {
     if (value == null)
       AlwaysThrow.ArgumentNullException(expression ?? nameof(value), caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void ArgumentIsNullOrEmpty<T>([NotNull] T[] value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) where T : class {
+    if (value == null)
+      AlwaysThrow.ArgumentNullException(expression ?? nameof(value), caller);
+    if (value.Length <= 0)
+      AlwaysThrow.ArgumentException(expression ?? nameof(value), $@"Parameter ""{expression ?? nameof(value)}"" must contains elements", caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void ArgumentIsNullOrEmpty<T>([NotNull] IEnumerable<T> value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) where T : class {
+    if (value == null)
+      AlwaysThrow.ArgumentNullException(expression ?? nameof(value), caller);
+    if (!value.Any())
+      AlwaysThrow.ArgumentException(expression ?? nameof(value), $@"Parameter ""{expression ?? nameof(value)}"" must contains elements", caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void ArgumentIsNullOrEmpty([NotNull] string value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value == null)
+      AlwaysThrow.ArgumentNullException(expression ?? nameof(value), caller);
+    if (value.Length <= 0)
+      AlwaysThrow.ArgumentException(expression ?? nameof(value), $@"Parameter ""{expression ?? nameof(value)}"" must contain contents", caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void ArgumentIsNullOrWhiteSpace([NotNull] string value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value == null)
+      AlwaysThrow.ArgumentNullException(expression ?? nameof(value), caller);
+    if (value.Length <= 0)
+      AlwaysThrow.ArgumentException(expression ?? nameof(value), $@"Parameter ""{expression ?? nameof(value)}"" must contain contents", caller);
+    
+    if (value.Any(c => !char.IsWhiteSpace(c)))
+      return;
+
+    AlwaysThrow.ArgumentException(expression ?? nameof(value), $@"Parameter ""{expression ?? nameof(value)}"" must contain non-whitespace characters", caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void IndexOutOfRange(int value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value < 0)
+      AlwaysThrow.IndexTooLowException(expression ?? nameof(value), value, caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void IndexOutOfRange(int value, int maxValue, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value < 0)
+      AlwaysThrow.IndexTooLowException(expression ?? nameof(value), value, caller);
+    if (value > maxValue)
+      AlwaysThrow.IndexTooHighException(expression ?? nameof(value), value, maxValue, caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void IndexOutOfRange(long value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value < 0)
+      AlwaysThrow.IndexTooLowException(expression ?? nameof(value), value, caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void IndexOutOfRange(long value, long maxValue, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value < 0)
+      AlwaysThrow.IndexTooLowException(expression ?? nameof(value), value, caller);
+    if (value > maxValue)
+      AlwaysThrow.IndexTooHighException(expression ?? nameof(value), value, maxValue, caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void CountOutOfRange(int value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value < 0)
+      AlwaysThrow.CountTooLowException(expression ?? nameof(value), value, caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void CountOutOfRange(int value, int maxValue, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value < 0)
+      AlwaysThrow.CountTooLowException(expression ?? nameof(value), value, caller);
+    if (value > maxValue)
+      AlwaysThrow.CountTooHighException(expression ?? nameof(value), value, maxValue, caller);
+  }
+
+  [DebuggerHidden]
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static void CountOutOfRange(int value, int checkValue, int maxValue, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) {
+    if (value < 0)
+      AlwaysThrow.CountTooLowException(expression ?? nameof(value), value, caller);
+    if (checkValue > maxValue)
+      AlwaysThrow.CountTooHighException(expression ?? nameof(value), value, checkValue, maxValue, caller);
   }
 
 }
--- Corlib.Extensions/Guard/AlwaysThrow.cs
+++ Corlib.Extensions/Guard/AlwaysThrow.cs
@@ -18,6 +18,26 @@ internal static class AlwaysThrow {
   [DebuggerHidden]
   [MethodImpl(MethodImplOptions.NoInlining)]
   [DoesNotReturn]
+  public static void ArgumentBelowRangeException<T>(string parameterName, T value, T minimum, [CallerMemberName] string caller = null) where T : struct => throw new ArgumentOutOfRangeException(parameterName, $@"{(caller == null ? string.Empty : caller + ":")}Parameter ""{parameterName}"" must not be below threshold. ({value} < {minimum})") { Data = { { parameterName, value }, { "Minimum", minimum } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
+  public static void ArgumentBelowOrEqualRangeException<T>(string parameterName, T value, T minimum, [CallerMemberName] string caller = null) where T : struct => throw new ArgumentOutOfRangeException(parameterName, $@"{(caller == null ? string.Empty : caller + ":")}Parameter ""{parameterName}"" must not be below or equal to threshold. ({value} <= {minimum})") { Data = { { parameterName, value }, { "Minimum", minimum } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
+  public static void ArgumentAboveRangeException<T>(string parameterName, T value, T maximum, [CallerMemberName] string caller = null) where T : struct => throw new ArgumentOutOfRangeException(parameterName, $@"{(caller == null ? string.Empty : caller + ":")}Parameter ""{parameterName}"" must not be above threshold. ({value} > {maximum})") { Data = { { parameterName, value }, { "Maximum", maximum } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
+  public static void ArgumentAboveOrEqualRangeException<T>(string parameterName, T value, T maximum, [CallerMemberName] string caller = null) where T : struct => throw new ArgumentOutOfRangeException(parameterName, $@"{(caller == null ? string.Empty : caller + ":")}Parameter ""{parameterName}"" must not be above or equal to threshold. ({value} >= {maximum})") { Data = { { parameterName, value }, { "Maximum", maximum } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
   public static void ArgumentException(string parameterName, string message, [CallerMemberName] string caller = null) => throw new ArgumentException($"{(caller == null ? string.Empty : caller + ":")}{message}", parameterName);
 
   [DebuggerHidden]
@@ -38,7 +58,22 @@ internal static class AlwaysThrow {
   [DebuggerHidden]
   [MethodImpl(MethodImplOptions.NoInlining)]
   [DoesNotReturn]
+  public static void IndexTooLowException(string parameterName, long value, [CallerMemberName] string caller = null) => throw new IndexOutOfRangeException($"{(caller == null ? string.Empty : caller + ":")}Index too low {value} < 0") { Data = { { parameterName, value } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
   public static void IndexTooHighException(string parameterName, int value, int maxValue, [CallerMemberName] string caller = null) => throw new IndexOutOfRangeException($"{(caller == null ? string.Empty : caller + ":")}Index too high {value} >= {maxValue}") { Data = { { parameterName, value }, { "Maximum", maxValue } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
+  public static void IndexTooHighException(string parameterName, long value, long maxValue, [CallerMemberName] string caller = null) => throw new IndexOutOfRangeException($"{(caller == null ? string.Empty : caller + ":")}Index too high {value} >= {maxValue}") { Data = { { parameterName, value }, { "Maximum", maxValue } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
+  public static void IndexOutOfRangeException([CallerMemberName] string caller = null) => throw new IndexOutOfRangeException($"{(caller == null ? string.Empty : caller + ":")}Index out of range");
 
   [DebuggerHidden]
   [MethodImpl(MethodImplOptions.NoInlining)]
@@ -49,6 +84,11 @@ internal static class AlwaysThrow {
   [MethodImpl(MethodImplOptions.NoInlining)]
   [DoesNotReturn]
   public static void CountTooHighException(string parameterName, int value, int maxValue, [CallerMemberName] string caller = null) => throw new ArgumentOutOfRangeException(parameterName, $"{(caller == null ? string.Empty : caller + ":")}Count too high {value} >= {maxValue}") { Data = { { parameterName, value }, { "Maximum", maxValue } } };
+
+  [DebuggerHidden]
+  [MethodImpl(MethodImplOptions.NoInlining)]
+  [DoesNotReturn]
+  public static void CountTooHighException(string parameterName, int value, int checkValue, int maxValue, [CallerMemberName] string caller = null) => throw new ArgumentOutOfRangeException(parameterName, $"{(caller == null ? string.Empty : caller + ":")}Count too high {checkValue}({value}) >= {maxValue}") { Data = { { parameterName, value }, { "Maximum", maxValue }, { "Index", checkValue } } };
 
   [DebuggerHidden]
   [MethodImpl(MethodImplOptions.NoInlining)]
