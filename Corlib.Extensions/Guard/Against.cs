0cf8f4c34769c26327a57d2922b89711a3efe21b
Hawkynt <github@synthelicz.de>
2023-04-19 09:49:28 +0200

    + R# annotations

--- Corlib.Extensions/Guard/Against.cs
+++ Corlib.Extensions/Guard/Against.cs
@@ -1,4 +1,26 @@
-﻿namespace Guard;
+﻿#region (c)2010-2042 Hawkynt
+/*
+  This file is part of Hawkynt's .NET Framework extensions.
+
+    Hawkynt's .NET Framework extensions are free software: 
+    you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Hawkynt's .NET Framework extensions is distributed in the hope that 
+    it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+    the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Hawkynt's .NET Framework extensions.  
+    If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+// ReSharper disable UnusedMember.Global
+namespace Guard;
 
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,7 +42,7 @@ internal static partial class Against {
 #if SUPPORTS_INLINING
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-  public static void ArgumentIsNull<T>([NotNull] T value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) where T : class {
+  public static void ArgumentIsNull<T>([NotNull][JetBrains.Annotations.NoEnumeration] T value, [CallerMemberName] string caller = null, [CallerArgumentExpression(nameof(value))] string expression = null) where T : class {
     if (value == null)
       AlwaysThrow.ArgumentNullException(expression ?? nameof(value), caller);
   }
