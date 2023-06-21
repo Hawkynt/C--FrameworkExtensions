4a3bfc0df94eae645a6df4c1a92189aae47ebf47
Hawkynt <github@synthelicz.de>
2023-06-21 14:27:23 +0200

    + feature flag for WaitHandle.Dispose

---
+++ Backports/System/Threading/WaitHandle.cs
@@ -0,0 +1,43 @@
+﻿#region (c)2010-2042 Hawkynt
+/*
+  This file is part of Hawkynt's .NET Framework extensions.
+
+    Hawkynt's .NET Framework extensions are free software:
+    you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Hawkynt's .NET Framework extensions is distributed in the hope that
+    it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+    the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Hawkynt's .NET Framework extensions.
+    If not, see <http://www.gnu.org/licenses/>.
+*/
+#endregion
+
+namespace System.Threading;
+
+using Reflection;
+
+#if COMPILE_TO_EXTENSION_DLL
+public
+#else
+internal
+#endif
+static partial class WaitHandleExtensions {
+
+#if !SUPPORTS_WAITHANDLE_DISPOSE
+  public static void Dispose(this WaitHandle @this) {
+    if (@this == null)
+      throw new NullReferenceException();
+
+    var method = @this.GetType().GetMethod("Dispose", BindingFlags.FlattenHierarchy | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+    method?.Invoke(@this, new object[] { });
+  }
+#endif
+
+}
