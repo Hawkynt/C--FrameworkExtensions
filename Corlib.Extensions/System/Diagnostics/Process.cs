6422046058e021cf71e831f7eeb5c82b0b013b11
Hawkynt <github@synthelicz.de>
2023-06-23 11:14:32 +0200

    + Children method

--- Corlib.Extensions/System/Diagnostics/Process.cs
+++ Corlib.Extensions/System/Diagnostics/Process.cs
@@ -29,108 +29,196 @@ using System.Runtime.InteropServices;
 #if SUPPORTS_INLINING
 using System.Runtime.CompilerServices;
 #endif
+using Guard;
+namespace System.Diagnostics;
 
 // ReSharper disable PartialTypeWithSinglePart
 // ReSharper disable UnusedMember.Global
 // ReSharper disable MemberCanBePrivate.Global
-namespace System.Diagnostics {
+
 
 #if COMPILE_TO_EXTENSION_DLL
-  public
+public
 #else
-  internal
+internal
 #endif
-  static partial class ProcessExtensions {
+static partial class ProcessExtensions {
+
+  private static class NativeMethods {
+
     /// <summary>     
-    /// A utility class to determine a process parent.     
+    /// A utility class to determine a this parent.     
     /// </summary>     
     [StructLayout(LayoutKind.Sequential)]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Local")]
     [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
-    private struct ProcessBasicInformation {
+    public struct ProcessBasicInformation {
       // These members must match PROCESS_BASIC_INFORMATION         
-      private IntPtr Reserved1;
-      private IntPtr PebBaseAddress;
-      private IntPtr Reserved2_0;
-      private IntPtr Reserved2_1;
-      private IntPtr UniqueProcessId;
-      internal IntPtr InheritedFromUniqueProcessId;
+      public IntPtr Reserved1;
+      public IntPtr PebBaseAddress;
+      public IntPtr Reserved2_0;
+      public IntPtr Reserved2_1;
+      public IntPtr UniqueProcessId;
+      public IntPtr InheritedFromUniqueProcessId;
     }
 
-    [DllImport("ntdll.dll", EntryPoint = "NtQueryInformationProcess")]
+    [DllImport("ntdll.dll", EntryPoint = "NtQueryInformationProcess", SetLastError = true)]
     private static extern int _NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref ProcessBasicInformation processInformation, int processInformationLength, out int returnLength);
 
-    /// <summary>
-    /// Gets the parent process of the given process.
-    /// </summary>
-    /// <param name="This">This Process.</param>
-    /// <returns>The parent process or <c>null</c>.</returns>
-    public static Process Parent(this Process This) {
-#if SUPPORTS_CONTRACTS
-      Contract.Requires(This != null);
-#endif
-      try {
-        return (GetParentProcess(This.Handle));
-      } catch {
-        return (null);
-      }
-    }
-
-    /// <summary>
-    /// Recursively gets all parent processes of the given process.
-    /// </summary>
-    /// <param name="This">This Process.</param>
-    /// <returns></returns>
-    public static IEnumerable<Process> Parents(this Process This) {
-      var currentChild = This;
-      var dontSkip = false;
-      while (currentChild != null) {
-        if (dontSkip)
-          yield return currentChild;
-        else
-          dontSkip = true;
-        currentChild = currentChild.Parent();
-      }
-    }
-    /// <summary>        
-    /// Gets the parent process of the current process.      
-    /// </summary>        
-    /// <returns>An instance of the Process class.</returns>    
-#if SUPPORTS_INLINING
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-    public static Process GetParentProcess() => GetParentProcess(Process.GetCurrentProcess().Handle);
-
-    /// <summary>       
-    /// Gets the parent process of specified process.  
-    /// </summary>    
-    /// <param name="id">The process id.</param>   
-    /// <returns>An instance of the Process class.</returns>   
-#if SUPPORTS_INLINING
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-#endif
-    public static Process GetParentProcess(int id) => GetParentProcess(Process.GetProcessById(id).Handle);
-
-    /// <summary>   
-    /// Gets the parent process of a specified process.   
-    /// </summary>        
-    /// <param name="handle">The process handle.</param>   
-    /// <returns>An instance of the Process class.</returns>  
-    public static Process GetParentProcess(IntPtr handle) {
-      var pbi = new ProcessBasicInformation();
-      int returnLength;
-      var status = _NtQueryInformationProcess(handle, 0, ref pbi, Marshal.SizeOf(pbi), out returnLength);
-
+    public static ProcessBasicInformation NtQueryInformationProcess(IntPtr processHandle, int processInformationClass = 0) {
+      var result = new ProcessBasicInformation();
+      var status = _NtQueryInformationProcess(processHandle, processInformationClass, ref result, Marshal.SizeOf(result), out _);
       if (status != 0)
-        throw new Win32Exception(status);
+        throw new Win32Exception();
 
-      try {
-        return (Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32()));
-      } catch (ArgumentException) {
-
-        // not found               
-        return (null);
-      }
+      return result;
     }
 
   }
+
+  /// <summary>
+  /// Gets the parent <see cref="Process"/> of the given <see cref="Process"/>.
+  /// </summary>
+  /// <param name="this">This <see cref="Process"/>.</param>
+  /// <returns>The parent <see cref="Process"/> or <see langword="null"/>.</returns>
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static Process Parent(this Process @this) {
+    Against.ThisIsNull(@this);
+    return GetParentProcessOrNull(@this);
+  }
+
+  /// <summary>
+  /// Recursively gets all parent processes of the given <see cref="Process"/>.
+  /// </summary>
+  /// <param name="this">This <see cref="Process"/>.</param>
+  /// <returns>An enumeration of <see cref="Process"/>es</returns>
+  public static IEnumerable<Process> Parents(this Process @this) {
+    Against.ThisIsNull(@this);
+
+    var currentChild = @this;
+    for (;;) {
+      currentChild = GetParentProcessOrNull(currentChild);
+      if (currentChild == null)
+        yield break;
+
+      yield return currentChild;
+    }
+  }
+
+  /// <summary>
+  /// Get the direct child processes of the given <see cref="Process"/>.
+  /// </summary>
+  /// <param name="this">This <see cref="Process"/></param>
+  /// <returns>An enumeration of <see cref="Process"/>es</returns>
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static IEnumerable<Process> Children(this Process @this) {
+    Against.ThisIsNull(@this);
+
+    var id = @this.Id;
+    return Process.GetProcesses().Where(p => p.GetParentProcessOrNull()?.Id == id);
+  }
+
+  /// <summary>        
+  /// Gets the parent <see cref="Process"/> of the current one.
+  /// </summary>        
+  /// <returns>An instance of the <see cref="Process"/>-class.</returns>    
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static Process GetParentProcess() => GetParentProcess(Process.GetCurrentProcess());
+
+  /// <summary>       
+  /// Gets the parent <see cref="Process"/> of the given process id or throws.
+  /// </summary>    
+  /// <param name="processId">The process id.</param>   
+  /// <returns>An instance of the <see cref="Process"/> class.</returns>   
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static Process GetParentProcess(int processId) => GetParentProcess(Process.GetProcessById(processId));
+
+  /// <summary>       
+  /// Gets the parent <see cref="Process"/> of the given process id or throws.
+  /// </summary>    
+  /// <param name="processId">The process id.</param>   
+  /// <returns>An instance of the <see cref="Process"/> class or <see langword="null"/>.</returns>   
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static Process GetParentProcessOrNull(int processId) {
+    Process process;
+    try {
+      process = Process.GetProcessById(processId);
+    } catch (ArgumentException /* process not found */ ) {
+      return null;
+    }
+
+    return GetParentProcessOrNull(process);
+  }
+
+  /// <summary>
+  /// Gets the parent process of the given process or throws.
+  /// </summary>
+  /// <param name="this">This <see cref="Process"/></param>
+  /// <returns>Another <see cref="Process"/>-Instance</returns>
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static Process GetParentProcess(this Process @this) {
+    Against.ThisIsNull(@this);
+    return GetParentProcess(@this.Handle);
+  }    
+  
+  /// <summary>
+  /// Gets the parent process of the given process if any.
+  /// </summary>
+  /// <param name="this">This <see cref="Process"/></param>
+  /// <returns>Another <see cref="Process"/>-Instance or <see langword="null"/></returns>
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static Process GetParentProcessOrNull(this Process @this) {
+    Against.ThisIsNull(@this);
+    try {
+      return GetParentProcessOrNull(@this.Handle);
+    } catch (Exception) {
+      return null;
+    }
+  }
+
+  /// <summary>   
+  /// Gets the parent Process of a specified process handle.
+  /// </summary>        
+  /// <param name="handle">The process handle.</param>   
+  /// <returns>An instance of the <see cref="Process"/> class.</returns>  
+#if SUPPORTS_INLINING
+  [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+  public static Process GetParentProcess(IntPtr handle) {
+    var pbi = NativeMethods.NtQueryInformationProcess(handle);
+    return Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32());
+  }
+
+  /// <summary>   
+  /// Gets the parent Process of a specified process handle or <see langword="null"/>.
+  /// </summary>        
+  /// <param name="handle">The this handle.</param>   
+  /// <returns>An instance of the Process class or <see langword="null"/>.</returns>
+  public static Process GetParentProcessOrNull(IntPtr handle) {
+    try {
+      var pbi = NativeMethods.NtQueryInformationProcess(handle);
+      return Process.GetProcessById(pbi.InheritedFromUniqueProcessId.ToInt32());
+    } catch (Win32Exception /* can not find process */) {
+      return null;
+    } catch (InvalidOperationException /* process exited */) {
+      return null;
+    } catch (ArgumentException /* process no longer alive */) {
+      return null;
+    }
+  }
+
 }
