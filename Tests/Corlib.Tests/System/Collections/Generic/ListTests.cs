5d3ae39f3488ffb5952347c4f46c85cbfee5fa5b
Hawkynt <github@synthelicz.de>
2023-07-24 12:04:12 +0200

    + testing list .Any()

--- Tests/Corlib.Tests/System/Collections/Generic/ListTests.cs
+++ Tests/Corlib.Tests/System/Collections/Generic/ListTests.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 
-using static Corlib.Tests.NUnit.TestUtilities;
 using NUnit.Framework;
+using static Corlib.Tests.NUnit.TestUtilities;
+
 namespace Corlib.Tests.System.Collections.Generic;
 
 [TestFixture]
@@ -14,39 +15,46 @@ public class ListTests {
   [TestCase("", false, null)]
   [TestCase("a", true, "a")]
   [TestCase("a|b", true, "a")]
-  public void TryGetFirst(string? input,bool result,string? expected,Type? exception=null) {
-    ExecuteTest(()=> {
+  public void TryGetFirst(string? input,bool result,string? expected,Type? exception=null) 
+    => ExecuteTest(()=> {
       string? v = null;
       var r = input == null ? ((List<string?>)null!).TryGetFirst(out v) : ConvertFromStringToTestArray(input)?.ToList().TryGetFirst(out v);
       return (r,v);
-    },(result,expected),exception);
-  }
+    },(result,expected),exception)
+    ;
 
   [Test]
   [TestCase(null, false, null, typeof(NullReferenceException))]
   [TestCase("", false, null)]
   [TestCase("a", true, "a")]
   [TestCase("a|b", true, "b")]
-  public void TryGetLast(string? input, bool result, string? expected, Type? exception = null) {
-    ExecuteTest(() => {
+  public void TryGetLast(string? input, bool result, string? expected, Type? exception = null) 
+    => ExecuteTest(() => {
       string? v = null;
       var r = input == null ? ((List<string?>)null!).TryGetLast(out v) : ConvertFromStringToTestArray(input)?.ToList().TryGetLast(out v);
       return (r, v);
-    }, (result, expected), exception);
-  }
+    }, (result, expected), exception)
+    ;
 
   [Test]
   [TestCase(null, 0, false, null, typeof(NullReferenceException))]
   [TestCase("", -1, false, null, typeof(IndexOutOfRangeException))]
   [TestCase("a", 1, false, null)]
   [TestCase("a|b|c", 1, true, "b")]
-  public void TryGetItem(string? input, int index, bool result, string? expected, Type? exception = null) {
-    ExecuteTest(() => {
+  public void TryGetItem(string? input, int index, bool result, string? expected, Type? exception = null) 
+    => ExecuteTest(() => {
       string? v = null;
       var r = input == null ? ((List<string?>)null!).TryGetItem(index, out v) : ConvertFromStringToTestArray(input)?.ToList().TryGetItem(index, out v);
       return (r, v);
-    }, (result, expected), exception);
-  }
+    }, (result, expected), exception)
+    ;
 
+  [Test]
+  [TestCase(null, false, typeof(NullReferenceException))]
+  [TestCase("", false)]
+  [TestCase("a", true)]
+  public void Any(string? input, bool expected, Type? exception = null)
+    => ExecuteTest(() => input == null ? ((List<string?>)null!).Any() : ConvertFromStringToTestArray(input)?.ToList().Any(), expected, exception)
+    ;
+  
 }
-
