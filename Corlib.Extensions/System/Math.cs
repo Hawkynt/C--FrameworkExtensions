564205b50d646028996f3a5236dc984121797472
Hawkynt <github@synthelicz.de>
2023-08-25 15:39:51 +0200

    + EnumeratePrimes

--- Corlib.Extensions/System/Math.cs
+++ Corlib.Extensions/System/Math.cs
@@ -25,6 +25,10 @@ using System.Diagnostics.Contracts;
 #if SUPPORTS_INLINING
 using System.Runtime.CompilerServices;
 #endif
+using System.Collections.Generic;
+#if SUPPORTS_ASYNC
+using System.Threading.Tasks;
+#endif
 
 // ReSharper disable UnusedMember.Global
 // ReSharper disable RedundantCast
@@ -401,5 +405,255 @@ static partial class MathEx {
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
   public static double Acsc(this double @this) => Math.Asin(1 / @this);
+
+  /// <summary>
+  /// Enumerates all primes in the ulong value space.
+  /// </summary>
+  public static IEnumerable<ulong> EnumeratePrimes => _EnumeratePrimes();
+  
+  private readonly struct PrimeSieve {
+    private readonly ulong[] _values;
+    public PrimeSieve(ulong[] values) => this._values = values;
+
+    public IEnumerable<ulong> Enumerate() {
+      ulong prime=3;
+      var values = this._values;
+      for (var i = 0; i < values.Length; ++i,prime+=2) {
+        if (values[i]!=0)
+          continue;
+
+#if SUPPORTS_ASYNC
+        var task = this._FillSieveAsync(prime);
+        yield return prime;
+        task.Wait();
+#else
+        this._FillSieveAction(prime);
+        yield return prime;
+#endif
+      }
+    }
+
+#if SUPPORTS_ASYNC
+    private Task _FillSieveAsync(ulong prime) => Task.Factory.StartNew(this._FillSieveAction, prime);
+    private void _FillSieveAction(object state) => this._FillSieveAction((ulong)state);
+#endif
+
+    private void _FillSieveAction(ulong prime) {
+      var doublePrime = prime << 1;
+      var values = this._values;
+      var maxNumberInSieve = ((ulong)values.Length << 1) + 3;
+      for (var j = prime * prime; j < maxNumberInSieve; j += doublePrime)
+        values[(int)((j - 3) >> 1)] = j;
+    }
+    
+  }
+
+  private struct KnownPrimesStorage {
+    private readonly ulong[] _primes;
+    private int _index;
+
+    // no checks because only used internally and guaranteed to have a primes!=null && primes.Length > 0 
+    public KnownPrimesStorage(ulong[] primes) => this._primes = primes;
+
+    private bool _IsSpaceInBufferLeft() => this._index < this._primes.Length;
+
+    // no checks because we guarantee, that all calls occur while there is still space in the array
+    public void Add(ulong prime) => this._primes[this._index++] = prime;
+
+    public IEnumerable<ulong> Enumerate() {
+      foreach (var prime in this._GenerateAndFillBuffer())
+        yield return prime;
+
+#if COLOR_PRIME_GENERATION
+      Console.ForegroundColor = ConsoleColor.Yellow;
+#endif
+
+      foreach (var prime in this._EnumerateWithFullBuffer())
+        yield return prime;
+    }
+
+    private IEnumerable<ulong> _GenerateAndFillBuffer() {
+      
+      // array always valid
+      var primes = this._primes;
+
+      // array always contains at least one prime from the sieve
+      var lastKnownPrime = primes[this._index - 1];
+
+#if SUPPORTS_ASYNC
+      var task = Task.Factory.StartNew(this._FindNextPrimeWithPartiallyFilledBuffer, lastKnownPrime);
+      for(;;) {
+        task.Wait();
+        lastKnownPrime = task.Result;
+        this.Add(lastKnownPrime);
+        if (this._IsSpaceInBufferLeft()) {
+          task = Task.Factory.StartNew(this._FindNextPrimeWithPartiallyFilledBuffer, lastKnownPrime);
+          yield return lastKnownPrime;
+        } else {
+          yield return lastKnownPrime;
+          yield break;
+        }
+      }
+#else
+      while (this._IsSpaceInBufferLeft()) {
+        lastKnownPrime = this._FindNextPrimeWithPartiallyFilledBuffer(lastKnownPrime);
+        this.Add(lastKnownPrime);
+        yield return lastKnownPrime;
+      }
+#endif
+    }
+
+#if SUPPORTS_ASYNC
+    private ulong _FindNextPrimeWithPartiallyFilledBuffer(object state) => this._FindNextPrimeWithPartiallyFilledBuffer((ulong)state);
+#endif
+
+    private ulong _FindNextPrimeWithPartiallyFilledBuffer(ulong lastKnownPrime) {
+      var candidate = lastKnownPrime;
+      do {
+        candidate += 2;
+      } while (!this._IsPrimeWithPartiallyFilledBuffer(candidate));
+
+      return candidate;
+    }
+
+    private bool _IsPrimeWithPartiallyFilledBuffer(ulong candidate) {
+      for (var i = 0; i < this._index; ++i)
+        if (candidate % this._primes[i] == 0)
+          return false;
+
+      return true;
+    }
+
+    private IEnumerable<ulong> _EnumerateWithFullBuffer() {
+      var lastKnownPrime = this._primes[^1];
+      var upperPrimeSquare = lastKnownPrime * lastKnownPrime;
+
+#if SUPPORTS_ASYNC
+      var task = Task.Factory.StartNew(this._FindNextPrimeWithFullBuffer, lastKnownPrime);
+      for (var candidate = lastKnownPrime + 2; candidate <= upperPrimeSquare; candidate = task.Result) {
+        task.Wait();
+        lastKnownPrime = task.Result;
+
+        if (lastKnownPrime <= upperPrimeSquare) {
+          task = Task.Factory.StartNew(this._FindNextPrimeWithFullBuffer, lastKnownPrime);
+          yield return lastKnownPrime;
+        } else {
+          yield return lastKnownPrime;
+          yield break;
+        }
+      }
+#else
+    for (var candidate = lastKnownPrime + 2; candidate <= upperPrimeSquare; candidate += 2) {
+        if (this._IsPrimeWithFullBuffer(candidate))
+            yield return candidate;
+    }
+#endif
+    }
+
+#if SUPPORTS_ASYNC
+    private ulong _FindNextPrimeWithFullBuffer(object state) => this._FindNextPrimeWithFullBuffer((ulong)state);
+#endif
+
+    private ulong _FindNextPrimeWithFullBuffer(ulong lastKnownPrime) {
+      var candidate = lastKnownPrime;
+      do {
+        candidate += 2;
+      } while (!this._IsPrimeWithFullBuffer(candidate));
+
+      return candidate;
+    }
+
+    private bool _IsPrimeWithFullBuffer(ulong candidate) {
+      foreach (var prime in this._primes)
+        if (candidate % prime == 0)
+          return false;
+
+      return true;
+    }
+
+  }
+
+  private static IEnumerable<ulong> _EnumeratePrimes() {
+#if COLOR_PRIME_GENERATION
+    Console.ForegroundColor= ConsoleColor.White;
+#endif
+    yield return 2;
+
+    var buffer = new ulong[128];
+    var sieve = new PrimeSieve(buffer);
+    var knownPrimes=new KnownPrimesStorage(buffer);
+
+#if COLOR_PRIME_GENERATION
+    Console.ForegroundColor = ConsoleColor.Cyan;
+#endif
+    foreach (var prime in sieve.Enumerate()) {
+      knownPrimes.Add(prime);
+      yield return prime;
+    }
+
+#if COLOR_PRIME_GENERATION
+    Console.ForegroundColor = ConsoleColor.Green;
+#endif
+    foreach (var prime in knownPrimes.Enumerate())
+      yield return prime;
+
+#if COLOR_PRIME_GENERATION
+    Console.ForegroundColor = ConsoleColor.Red;
+#endif
+    foreach (var prime in EnumerateSlowPrimesWithKnowns())
+      yield return prime;
+
+    IEnumerable<ulong> EnumerateSlowPrimesWithKnowns() {
+      var largestKnownPrime = buffer[^1];
+
+      // Start from the square of the last known prime plus 2 (to ensure it's odd)
+      var candidate = largestKnownPrime * largestKnownPrime + 2;
+
+#if SUPPORTS_ASYNC
+      var task = Task.Factory.StartNew(_IsPrimeWithBufferAndBeyondT,candidate);
+      for (;;) {
+        task.Wait();
+        var isPrime = task.Result;
+
+        if (isPrime)
+          yield return candidate;
+
+        candidate += 2; // Ensure we only check odd numbers
+        task = Task.Factory.StartNew( _IsPrimeWithBufferAndBeyondT,candidate);
+      }
+#else
+      for (;;) {
+        var isPrime = _IsPrimeWithBufferAndBeyond(candidate);
+
+        if (isPrime)
+          yield return candidate;
+
+        candidate += 2; // Ensure we only check odd numbers
+      }
+#endif
+    }
+
+#if SUPPORTS_ASYNC
+    bool _IsPrimeWithBufferAndBeyondT(object state) => _IsPrimeWithBufferAndBeyond((ulong)state);
+#endif
+
+    bool _IsPrimeWithBufferAndBeyond(ulong candidate) {
+      // 1. Check divisibility with all primes in the buffer
+      foreach (var prime in buffer)
+        if (candidate % prime == 0)
+          return false;
+
+      // 2. If none of the primes in the buffer divide the candidate, 
+      //    check divisibility with numbers (only odd ones) up to the square root of the candidate
+      var sqrtCandidate = (ulong)Math.Sqrt(candidate);
+      for (var i = buffer[^1] + 2; i <= sqrtCandidate; i += 2)
+        if (candidate % i == 0)
+          return false;
+
+      return true;
+    }
+
+
+  }
 
 }
