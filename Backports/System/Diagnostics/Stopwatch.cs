e4c2ccc65d49730723763caacb74c24e1523f657
Hawkynt <github@synthelicz.de>
2024-04-16 10:34:01 +0200

    + Stopwatch Restart polyfill

---
+++ Backports/System/Diagnostics/Stopwatch.cs
@@ -0,0 +1,23 @@
+﻿namespace System.Diagnostics;
+
+// ReSharper disable once UnusedMember.Global
+public static class StopwatchPolyfills {
+
+#if !SUPPORTS_STOPWATCH_RESTART
+
+  /// <summary>
+  /// Stops time interval measurement, resets the elapsed time to zero, and starts measuring elapsed time.
+  /// </summary>
+  /// <param name="this">This <see cref="Stopwatch"/></param>
+  public static void Restart(this Stopwatch @this) {
+    if (@this == null)
+      throw new NullReferenceException();
+    
+    @this.Reset();
+    @this.Start();
+  }
+
+#endif
+
+}
+
