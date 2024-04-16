af6619c05940ff403f4a3170a63109401a3c3d75
Hawkynt <github@synthelicz.de>
2024-04-16 10:57:33 +0200

    * refac

--- Backports/System/Collections/Generic/IReadOnlyDictionary.cs
+++ Backports/System/Collections/Generic/IReadOnlyDictionary.cs
@@ -1,42 +1,46 @@
-﻿using System.Collections.Generic;
+﻿#region (c)2010-2042 Hawkynt
 
-#if !SUPPORTS_READ_ONLY_COLLECTIONS
-#region (c)2010-2042 Hawkynt
 /*
   This file is part of Hawkynt's .NET Framework extensions.
 
-    Hawkynt's .NET Framework extensions are free software: 
+    Hawkynt's .NET Framework extensions are free software:
     you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Hawkynt's .NET Framework extensions is distributed in the hope that 
-    it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+    Hawkynt's .NET Framework extensions is distributed in the hope that
+    it will be useful, but WITHOUT ANY WARRANTY; without even the implied
     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
     the GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Hawkynt's .NET Framework extensions.  
+    along with Hawkynt's .NET Framework extensions.
     If not, see <http://www.gnu.org/licenses/>.
 */
+
 #endregion
+
+using System.Collections.Generic;
+
+#if !SUPPORTS_READ_ONLY_COLLECTIONS
 
 namespace System.Collections.ObjectModel;
 
+// ReSharper disable UnusedMember.Global
 #if COMPILE_TO_EXTENSION_DLL
 public
 #else
-  internal
+internal
 #endif
-  interface IReadOnlyDictionary<K, V> : IDictionary<K, V> { }
+interface IReadOnlyDictionary<K, V> : IDictionary<K, V> { }
 
 #if COMPILE_TO_EXTENSION_DLL
 public
 #else
-  internal
+internal
 #endif
-  class ReadOnlyDictionary<K, V> : Dictionary<K, V>, IReadOnlyDictionary<K, V> {
+class ReadOnlyDictionary<K, V> : Dictionary<K, V>, IReadOnlyDictionary<K, V> {
 
   public ReadOnlyDictionary(IDictionary<K,V> dictionary) : base(dictionary) {}
 }
