fe8c531d6b5a694103f035ff6426f04496fe9da4
Hawkynt <github@synthelicz.de>
2024-05-02 14:56:03 +0200

    + utilities for stuff that can not be done by polyfills

---
+++ Corlib.Extensions/Utilities/Array.cs
@@ -0,0 +1,20 @@
+﻿namespace Utilities;
+internal class Array {
+
+#if !SUPPORTS_ARRAY_EMPTY
+
+  private static class EmptyArray<T> {
+    public static readonly T[] Empty = new T[0];
+  }
+  
+#endif
+
+  public static T[] Empty<T>()
+#if SUPPORTS_ARRAY_EMPTY
+    => System.Array.Empty<T>()
+#else
+    => EmptyArray<T>.Empty
+#endif
+    ;
+  
+}
