220012be50c1402baa71d00844a7a606f794853e
Hawkynt <github@synthelicz.de>
2024-05-08 14:36:03 +0200

    * refac

--- Tests/Corlib.Tests/System/DateTimeTests.cs
+++ Tests/Corlib.Tests/System/DateTimeTests.cs
@@ -9,51 +9,36 @@ public class DateTimeTests {
     ? result
     : DateTime.ParseExact(text, "yyyyMMdd", CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal);
 
-  [Test]
-  public void TestStartOfWeek() {
-    var testcases = new[] {
-      Tuple.Create("20191027", "20191021000000",DayOfWeek.Monday),
-      Tuple.Create("20191026","20191021000000",DayOfWeek.Monday),
-      Tuple.Create("20191025","20191021000000",DayOfWeek.Monday),
-      Tuple.Create("20191024","20191021000000",DayOfWeek.Monday),
-      Tuple.Create("20191023","20191021000000",DayOfWeek.Monday),
-      Tuple.Create("20191022","20191021000000",DayOfWeek.Monday),
-      Tuple.Create("20191021000000","20191021000000",DayOfWeek.Monday)
-    };
-
-    foreach (var @case in testcases) {
-      var input = Parse(@case.Item1);
-      var expected = Parse(@case.Item2);
-      var startDayOfWeek = @case.Item3;
-      var result = input.StartOfWeek(startDayOfWeek);
-      Assert.AreEqual(result, expected);
-    }
+  [TestCase("20191027", "20191021000000", DayOfWeek.Monday)]
+  [TestCase("20191026", "20191021000000", DayOfWeek.Monday)]
+  [TestCase("20191025", "20191021000000", DayOfWeek.Monday)]
+  [TestCase("20191024", "20191021000000", DayOfWeek.Monday)]
+  [TestCase("20191023", "20191021000000", DayOfWeek.Monday)]
+  [TestCase("20191022", "20191021000000", DayOfWeek.Monday)]
+  [TestCase("20191021000000", "20191021000000", DayOfWeek.Monday)]
+  public void TestStartOfWeek(string inputDate, string expectedDate, DayOfWeek startDayOfWeek) {
+    var input = Parse(inputDate);
+    var expected = Parse(expectedDate);
+    var result = input.StartOfWeek(startDayOfWeek);
+    Assert.AreEqual(expected, result);
   }
 
-  [Test]
-  public void TestDayInCurrentWeek() {
-    var testcases = new[] {
-      Tuple.Create("20191027", "20191021000000",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191026","20191021000000",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191025","20191021000000",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191024","20191021000000",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191023","20191021000000",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191022","20191021000000",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191021000000","20191021000000",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191107","20191104",DayOfWeek.Monday,DayOfWeek.Monday),
-      Tuple.Create("20191107","20191105",DayOfWeek.Tuesday,DayOfWeek.Monday),
-      Tuple.Create("20191107","20191110",DayOfWeek.Sunday,DayOfWeek.Monday),
-      Tuple.Create("20191107","20191103",DayOfWeek.Sunday,DayOfWeek.Sunday),
-    };
-
-    foreach (var @case in testcases) {
-      var input = Parse(@case.Item1);
-      var expected = Parse(@case.Item2);
-      var startDayOfWeek = @case.Item4;
-      var dayOfWeek = @case.Item3;
-      var result = input.DayInCurrentWeek(dayOfWeek, startDayOfWeek);
-      Assert.AreEqual(result, expected);
-    }
+  [TestCase("20191027", "20191021000000", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191026", "20191021000000", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191025", "20191021000000", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191024", "20191021000000", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191023", "20191021000000", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191022", "20191021000000", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191021000000", "20191021000000", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191107", "20191104", DayOfWeek.Monday, DayOfWeek.Monday)]
+  [TestCase("20191107", "20191105", DayOfWeek.Tuesday, DayOfWeek.Monday)]
+  [TestCase("20191107", "20191110", DayOfWeek.Sunday, DayOfWeek.Monday)]
+  [TestCase("20191107", "20191103", DayOfWeek.Sunday, DayOfWeek.Sunday)]
+  public void TestDayInCurrentWeek(string inputDate, string expectedDate, DayOfWeek dayOfWeek, DayOfWeek startDayOfWeek) {
+    var input = Parse(inputDate);
+    var expected = Parse(expectedDate);
+    var result = input.DayInCurrentWeek(dayOfWeek, startDayOfWeek);
+    Assert.AreEqual(expected, result);
   }
 
 
