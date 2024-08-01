e05c28fa917ebc5a8d6fbc990790e03df6798d04
Hawkynt <github@synthelicz.de>
2024-08-01 09:06:37 +0200

    * refac

--- System.Windows.Forms.Extensions/System/Drawing/Image.cs
+++ System.Windows.Forms.Extensions/System/Drawing/Image.cs
@@ -26,6 +26,70 @@ using Guard;
 namespace System.Drawing;
 
 public static partial class ImageExtensions {
+
+  /// <summary>
+  /// Prints the image using the system's printer dialog.
+  /// </summary>
+  /// <param name="this">The image to be printed.</param>
+  /// <param name="documentName">The name of the document to be printed. Optional.</param>
+  /// <param name="dialog">(Optional) The dialog to use for printing. If null, a new dialog will be created.</param>
+  /// <returns>The used printer settings.</returns>
+  /// <example>
+  /// <code>
+  /// // Prints an image using the system's printer dialog
+  /// Image image = new Bitmap("path_to_image.jpg");
+  /// PrinterSettings settings = image.PrintImageWithDialog("My Document");
+  /// </code>
+  /// </example>
+  public static PrinterSettings PrintImageWithDialog(this Image @this, string documentName = null, PrintDialog dialog = null) {
+    Against.ThisIsNull(@this);
+
+    using var document = _SetupDocument(@this, documentName, out var pageCount);
+
+    var needToCreateDialog = dialog == null;
+    try {
+      if (needToCreateDialog)
+        dialog = new();
+
+      dialog.PrinterSettings.MinimumPage = 1;
+      dialog.PrinterSettings.MaximumPage = pageCount;
+      dialog.Document = document;
+
+      if (dialog.ShowDialog() != DialogResult.OK)
+        return null;
+
+      document.Print();
+      return document.PrinterSettings;
+    } finally {
+      if (needToCreateDialog)
+        dialog?.Dispose();
+    }
+  }
+
+  /// <summary>
+  /// Prints the image without a printer dialog.
+  /// </summary>
+  /// <param name="this">The image to be printed.</param>
+  /// <param name="documentName">(Optional) The name of the document.</param>
+  /// <param name="settings">(Optional) The printer settings to be used.</param>
+  /// <example>
+  /// <code>
+  /// // Prints an image without a printer dialog
+  /// Image image = new Bitmap("path_to_image.jpg");
+  /// image.PrintImage("My Document", new PrinterSettings());
+  /// </code>
+  /// </example>
+  public static void PrintImage(this Image @this, string documentName = null, PrinterSettings settings = null) {
+    Against.ThisIsNull(@this);
+
+    using var document = _SetupDocument(@this, documentName, out _);
+
+    if (settings != null)
+      document.PrinterSettings = settings;
+
+    document.Print();
+  }
+
   /// <summary>
   ///   Gets a single page of a multipage image.
   /// </summary>
@@ -63,93 +127,41 @@ public static partial class ImageExtensions {
     return result;
   }
 
-  /// <summary>
-  ///   Prints the image using the system's printer dialog.
-  /// </summary>
-  /// <param name="this">This Image.</param>
-  /// <param name="documentName">Name of the document.</param>
-  /// <param name="dialog">The dialog to use; creates its own when none is given.</param>
-  /// <returns>The used printersettings</returns>
-  public static PrinterSettings PrintImageWithDialog(this Image @this, string documentName = null, PrintDialog dialog = null) {
-    Against.ThisIsNull(@this);
-
-    using var document = new PrintDocument();
-    var pageCount = _GetPageCount(@this);
-
-    var noDialog = dialog == null;
+  private static PrintDocument _SetupDocument(Image @this, string documentName, out int pageCount) {
+    PrintDocument document = null;
     try {
-      if (noDialog)
-        dialog = new();
+      document = new PrintDocument();
+      pageCount = _GetPageCount(@this);
 
       if (documentName != null)
         document.DocumentName = documentName;
 
       var currentPageIndex = 0;
-      document.PrintPage += (_, ea) => {
-        using (var currentPage = _GetPageAt(@this, currentPageIndex)) {
-          var imageIsLandscape = currentPage.Width > currentPage.Height;
-          var marginBounds = ea.PageBounds;
-          var paperIsLandscape = marginBounds.Width > marginBounds.Height;
-          Image printImage = null;
-          try {
-            printImage = imageIsLandscape == paperIsLandscape ? currentPage : _Rotate90(currentPage);
+      var maxPages = pageCount;
+      document.PrintPage += (_, ea) => _OnPrintPage(ea, @this, maxPages, ref currentPageIndex);
+      return document;
+    } catch {
+      document?.Dispose();
+      throw;
+    }
 
-            var ratio = printImage.Width / (double)printImage.Height;
-            if (marginBounds.Width / ratio > marginBounds.Height)
-              marginBounds.Width = (int)(marginBounds.Height * ratio);
-            else
-              marginBounds.Height = (int)(marginBounds.Width / ratio);
-
-            ea.Graphics.CompositingQuality = CompositingQuality.HighQuality;
-            ea.Graphics.DrawImage(printImage, marginBounds);
-          } finally {
-            if (imageIsLandscape != paperIsLandscape)
-              printImage?.Dispose();
-          }
+    static void _OnPrintPage(PrintPageEventArgs ea, Image @this, int pageCount, ref int currentPageIndex) {
+      Image currentPage = null;
+      try {
+        currentPage = _GetPageAt(@this, currentPageIndex);
+        if (currentPage == null) {
+          ea.HasMorePages = false;
+          return;
         }
 
-        ea.HasMorePages = ++currentPageIndex < pageCount;
-      };
-
-      dialog.PrinterSettings.MinimumPage = 1;
-      dialog.PrinterSettings.MaximumPage = pageCount;
-      dialog.Document = document;
-
-      if (dialog.ShowDialog() != DialogResult.OK)
-        return null;
-
-      document.Print();
-      return document.PrinterSettings;
-    } finally {
-      if (noDialog)
-        dialog?.Dispose();
-    }
-  }
-
-  /// <summary>
-  ///   Prints the image without a printer dialog.
-  /// </summary>
-  /// <param name="this">This Image.</param>
-  /// <param name="documentName">Name of the document.</param>
-  /// <param name="settings">The settings.</param>
-  public static void PrintImage(this Image @this, string documentName = null, PrinterSettings settings = null) {
-    Against.ThisIsNull(@this);
-
-    using var document = new PrintDocument();
-    var pageCount = _GetPageCount(@this);
-
-    if (documentName != null)
-      document.DocumentName = documentName;
-
-    var currentPageIndex = 0;
-    document.PrintPage += (_, ea) => {
-      using (var currentPage = _GetPageAt(@this, currentPageIndex)) {
-        var imageIsLandscape = currentPage.Width > currentPage.Height;
         var marginBounds = ea.PageBounds;
+        var imageIsLandscape = currentPage.Width > currentPage.Height;
         var paperIsLandscape = marginBounds.Width > marginBounds.Height;
+        var imageOrientationMatchesPaperOrientation = imageIsLandscape == paperIsLandscape;
+
         Image printImage = null;
         try {
-          printImage = imageIsLandscape == paperIsLandscape ? currentPage : _Rotate90(currentPage);
+          printImage = imageOrientationMatchesPaperOrientation ? currentPage : _Rotate90(currentPage);
 
           var ratio = printImage.Width / (double)printImage.Height;
           if (marginBounds.Width / ratio > marginBounds.Height)
@@ -160,17 +172,16 @@ public static partial class ImageExtensions {
           ea.Graphics.CompositingQuality = CompositingQuality.HighQuality;
           ea.Graphics.DrawImage(printImage, marginBounds);
         } finally {
-          if (imageIsLandscape != paperIsLandscape)
+          if (!imageOrientationMatchesPaperOrientation)
             printImage?.Dispose();
         }
+
+        ea.HasMorePages = ++currentPageIndex < pageCount;
+      } finally {
+        currentPage?.Dispose();
       }
+    }
 
-      ea.HasMorePages = ++currentPageIndex < pageCount;
-    };
-
-    if (settings != null)
-      document.PrinterSettings = settings;
-
-    document.Print();
   }
+
 }
