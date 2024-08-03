aa3891bab18f313d2db1581a8c618114be19e589
Hawkynt <github@synthelicz.de>
2024-08-03 10:55:27 +0200

    * docs

--- System.Windows.Forms.Extensions/System/Windows/Forms/Binding.cs
+++ System.Windows.Forms.Extensions/System/Windows/Forms/Binding.cs
@@ -22,12 +22,22 @@ using Guard;
 namespace System.Windows.Forms;
 
 public static partial class BindingExtensions {
+
   /// <summary>
-  ///   Adds a value converter for a certain type of values.
+  /// Adds a type converter to a <see cref="System.Windows.Forms.Binding"/>.
   /// </summary>
-  /// <typeparam name="TType">The type of the value to convert.</typeparam>
-  /// <param name="this">This Binding.</param>
-  /// <param name="converter">The converter.</param>
+  /// <typeparam name="TType">The type to which the values will be converted.</typeparam>
+  /// <param name="this">The <see cref="System.Windows.Forms.Binding"/> instance.</param>
+  /// <param name="converter">The function to convert the values.</param>
+  /// <exception cref="System.NullReferenceException">Thrown if <paramref name="this"/> is <see langword="null"/>.</exception>
+  /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="converter"/> is <see langword="null"/>.</exception>
+  /// <example>
+  /// <code>
+  /// Binding binding = new Binding("Text", dataSource, "Property");
+  /// binding.AddTypeConverter(value => Convert.ToInt32(value));
+  /// // The binding now includes a type converter that converts values to integers.
+  /// </code>
+  /// </example>
   public static void AddTypeConverter<TType>(this Binding @this, Func<object, TType> converter) {
     Against.ThisIsNull(@this);
     Against.ArgumentIsNull(converter);
--- System.Windows.Forms.Extensions/System/Windows/Forms/BindingsCollection.cs
+++ System.Windows.Forms.Extensions/System/Windows/Forms/BindingsCollection.cs
@@ -19,23 +19,56 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using Guard;
 
 namespace System.Windows.Forms;
 
 public static partial class BindingsCollectionExtensions {
-  /// <summary>
-  ///   Gets a binding by its bound property.
-  /// </summary>
-  /// <param name="this">This BindingCollection.</param>
-  /// <param name="propertyName">Name of the property.</param>
-  /// <returns>The first binding that matches the given property name.</returns>
-  public static Binding GetBindingByPropertyName(this BindingsCollection @this, string propertyName) => @this.GetBindingsByPropertyName(propertyName).First();
 
   /// <summary>
-  ///   Gets bindings by their bound property.
+  /// Retrieves the first binding in the <see cref="System.Windows.Forms.BindingsCollection"/> that matches the specified property name.
   /// </summary>
-  /// <param name="this">This BindingCollection.</param>
-  /// <param name="propertyName">Name of the property.</param>
-  /// <returns>A list of bindings that match the given property name.</returns>
-  public static IEnumerable<Binding> GetBindingsByPropertyName(this BindingsCollection @this, string propertyName) => @this.Cast<Binding>().Where(b => b.PropertyName == propertyName);
+  /// <param name="this">The <see cref="System.Windows.Forms.BindingsCollection"/> instance.</param>
+  /// <param name="propertyName">The name of the property to match.</param>
+  /// <returns>The first <see cref="System.Windows.Forms.Binding"/> that matches the specified property name.</returns>
+  /// <exception cref="System.NullReferenceException">Thrown if <paramref name="this"/> is <see langword="null"/>.</exception>
+  /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="propertyName"/> is <see langword="null"/> or white space.</exception>
+  /// <exception cref="System.InvalidOperationException">Thrown if no binding matches the specified property name.</exception>
+  /// <example>
+  /// <code>
+  /// BindingsCollection bindings = new BindingsCollection();
+  /// // Assuming bindings have been added to the collection
+  /// string propertyName = "Text";
+  /// Binding binding = bindings.GetBindingByPropertyName(propertyName);
+  /// Console.WriteLine(binding.PropertyName);
+  /// </code>
+  /// </example>
+  public static Binding GetBindingByPropertyName(this BindingsCollection @this, string propertyName) => GetBindingsByPropertyName(@this, propertyName).First();
+
+  /// <summary>
+  /// Retrieves all bindings in the <see cref="System.Windows.Forms.BindingsCollection"/> that match the specified property name.
+  /// </summary>
+  /// <param name="this">The <see cref="System.Windows.Forms.BindingsCollection"/> instance.</param>
+  /// <param name="propertyName">The name of the property to match.</param>
+  /// <returns>An <see cref="System.Collections.Generic.IEnumerable{Binding}"/> containing all bindings that match the specified property name.</returns>
+  /// <exception cref="System.NullReferenceException">Thrown if <paramref name="this"/> is <see langword="null"/>.</exception>
+  /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="propertyName"/> is <see langword="null"/> or white space.</exception>
+  /// <example>
+  /// <code>
+  /// BindingsCollection bindings = new BindingsCollection();
+  /// // Assuming bindings have been added to the collection
+  /// string propertyName = "Text";
+  /// IEnumerable&lt;Binding&gt; matchedBindings = bindings.GetBindingsByPropertyName(propertyName);
+  /// foreach (var binding in matchedBindings)
+  /// {
+  ///     Console.WriteLine(binding.PropertyName);
+  /// }
+  /// </code>
+  /// </example>
+  public static IEnumerable<Binding> GetBindingsByPropertyName(this BindingsCollection @this, string propertyName) {
+    Against.ThisIsNull(@this);
+    Against.ArgumentIsNullOrWhiteSpace(propertyName);
+
+    return @this.Cast<Binding>().Where(b => b.PropertyName == propertyName);
+  }
 }
