34246eb2a120a57ec70613d71abf436651b85a42
Hawkynt <github@synthelicz.de>
2024-12-18 15:02:53 +0100

    + SkipLocalsInit polyfill

---
+++ Backports/Features/SkipLocalsInit/System/Runtime/CompilerServices/SkipLocalsInitAttribute.cs
@@ -0,0 +1,27 @@
+#region (c)2010-2042 Hawkynt
+
+// This file is part of Hawkynt's .NET Framework extensions.
+// 
+// Hawkynt's .NET Framework extensions are free software:
+// you can redistribute and/or modify it under the terms
+// given in the LICENSE file.
+// 
+// Hawkynt's .NET Framework extensions is distributed in the hope that
+// it will be useful, but WITHOUT ANY WARRANTY without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the LICENSE file for more details.
+// 
+// You should have received a copy of the License along with Hawkynt's
+// .NET Framework extensions. If not, see
+// <https://github.com/Hawkynt/C--FrameworkExtensions/blob/master/LICENSE>.
+
+#endregion
+
+#if !SUPPORTS_SKIP_LOCALS_INIT
+
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Method | AttributeTargets.Module | AttributeTargets.Property | AttributeTargets.Struct, Inherited = false)]
+public sealed class SkipLocalsInitAttribute : Attribute;
+
+#endif
