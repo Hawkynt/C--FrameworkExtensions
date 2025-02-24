a2faa58630adb98e26dbcdfef1438c8443ec1dc6
Hawkynt <github@synthelicz.de>
2025-02-24 11:47:56 +0100

    # fixed a subtile bug and made it faster

--- Backports/Features/HasFlag/System/Enum.cs
+++ Backports/Features/HasFlag/System/Enum.cs
@@ -19,11 +19,53 @@
 
 #if !SUPPORTS_HAS_FLAG
 
+using System.Reflection.Emit;
+
 namespace System;
 
 public static partial class EnumPolyfills {
-  public static bool HasFlag<T>(this T @this, T flag) where T : Enum
-    => ((ulong)(object)@this & (ulong)(object)flag) == (ulong)(object)flag;
+  public static bool HasFlag<T>(this T @this, T flag) where T : unmanaged, Enum => HasFlagHelper<T>.HasFlag(@this, flag);
+
+  private static class HasFlagHelper<T> where T : unmanaged, Enum {
+    public static readonly Func<T, T, bool> HasFlag = _CreateImplementation();
+
+    private static Func<T, T, bool> _CreateImplementation() {
+      var underlyingType = Enum.GetUnderlyingType(typeof(T));
+      var dm = new DynamicMethod(string.Empty, typeof(bool), [typeof(T), typeof(T)], true);
+      var il = dm.GetILGenerator();
+      il.Emit(OpCodes.Ldarg_1);
+      EmitConversion(il, underlyingType);
+      il.Emit(OpCodes.Dup);
+      il.Emit(OpCodes.Ldarg_0);
+      EmitConversion(il, underlyingType);
+      il.Emit(OpCodes.And);
+      il.Emit(OpCodes.Ceq);
+      il.Emit(OpCodes.Ret);
+      return (Func<T, T, bool>)dm.CreateDelegate(typeof(Func<T, T, bool>));
+
+      static void EmitConversion(ILGenerator il, Type underlyingType) {
+        if (underlyingType == typeof(long))
+          il.Emit(OpCodes.Conv_I8);
+        else if (underlyingType == typeof(ulong))
+          il.Emit(OpCodes.Conv_U8);
+        else if(underlyingType == typeof(int))
+          il.Emit(OpCodes.Conv_I4);
+        else if (underlyingType == typeof(uint))
+          il.Emit(OpCodes.Conv_U4);
+        else if (underlyingType == typeof(short))
+          il.Emit(OpCodes.Conv_I2);
+        else if (underlyingType == typeof(ushort))
+          il.Emit(OpCodes.Conv_U2);
+        else if (underlyingType == typeof(sbyte))
+          il.Emit(OpCodes.Conv_I1);
+        else if (underlyingType == typeof(byte))
+          il.Emit(OpCodes.Conv_U1);
+        else 
+          throw new ArgumentException("Unsupported underlying enum type", nameof(underlyingType));
+      }
+    }
+  }
+
 }
 
 #endif
