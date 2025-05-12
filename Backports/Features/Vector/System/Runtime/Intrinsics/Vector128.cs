c7ac5f01d57a5868272181457edd6d644da542da
Hawkynt <github@synthelicz.de>
2025-05-12 16:59:00 +0200

    + minimum vectors

---
+++ Backports/Features/Vector/System/Runtime/Intrinsics/Vector128.cs
@@ -0,0 +1,26 @@
+﻿// This file is part of Hawkynt's .NET Framework extensions.
+//
+// Hawkynt's .NET Framework extensions are free software: 
+// you can redistribute and/or modify it under the terms 
+// given in the LICENSE file.
+//
+// Hawkynt's .NET Framework extensions is distributed in the hope that 
+// it will be useful, but WITHOUT ANY WARRANTY without even the implied 
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the LICENSE file for more details.
+//
+// You should have received a copy of the License along with Hawkynt's
+// .NET Framework extensions. If not, see
+// <https://github.com/Hawkynt/C--FrameworkExtensions/blob/master/LICENSE>.
+//
+
+#if !SUPPORTS_VECTOR_128
+
+namespace System.Runtime.Intrinsics;
+
+public struct Vector128<T> where T : unmanaged {
+  public static Vector128<T> Zero => default;
+  public static bool IsSupported => false;
+}
+
+#endif
