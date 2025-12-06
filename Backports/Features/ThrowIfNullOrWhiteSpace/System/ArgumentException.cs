412e9b13fbfdb2c818d9507b066df54759b6e373
Hawkynt <github@synthelicz.de>
2025-12-06 16:17:16 +0100

    + file split

--- Backports/Features/ThrowIfNullOrWhiteSpace/System/ArgumentException.cs
+++ Backports/Features/ThrowIfNullOrWhiteSpace/System/ArgumentException.cs
@@ -17,7 +17,7 @@
 
 #endregion
 
-#if !SUPPORTS_THROWIFNULLOREMPTY
+#if !SUPPORTS_THROWIFNULLORWHITESPACE
 
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
@@ -29,23 +29,39 @@ public static partial class ArgumentExceptionPolyfills {
   extension(ArgumentException) {
 
     /// <summary>
-    /// Throws an exception if <paramref name="argument"/> is <see langword="null"/> or empty.
+    /// Throws an exception if <paramref name="argument"/> is <see langword="null"/>, empty, or consists only of white-space characters.
     /// </summary>
-    /// <param name="argument">The string argument to validate as non-null and non-empty.</param>
+    /// <param name="argument">The string argument to validate.</param>
     /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
     /// <exception cref="ArgumentNullException"><paramref name="argument"/> is <see langword="null"/>.</exception>
-    /// <exception cref="ArgumentException"><paramref name="argument"/> is empty.</exception>
+    /// <exception cref="ArgumentException"><paramref name="argument"/> is empty or consists only of white-space characters.</exception>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static void ThrowIfNullOrEmpty([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null) {
-      if (string.IsNullOrEmpty(argument))
-        _ThrowNullOrEmpty(argument, paramName);
+    public static void ThrowIfNullOrWhiteSpace([NotNull] string? argument, [CallerArgumentExpression(nameof(argument))] string? paramName = null) {
+      if (_IsNullOrWhiteSpace(argument))
+        _ThrowNullOrWhiteSpace(argument, paramName);
     }
 
     [DoesNotReturn]
-    private static void _ThrowNullOrEmpty(string? argument, string? paramName) {
+    private static void _ThrowNullOrWhiteSpace(string? argument, string? paramName) {
       ArgumentNullException.ThrowIfNull(argument, paramName);
-      throw new ArgumentException("The value cannot be an empty string.", paramName);
+      throw new ArgumentException("The value cannot be an empty string or composed entirely of whitespace.", paramName);
     }
+
+#if SUPPORTS_STRING_IS_NULL_OR_WHITESPACE
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static bool _IsNullOrWhiteSpace(string? value) => string.IsNullOrWhiteSpace(value);
+#else
+    private static bool _IsNullOrWhiteSpace(string? value) {
+      if (value is null)
+        return true;
+
+      for (var i = 0; i < value.Length; ++i)
+        if (!char.IsWhiteSpace(value[i]))
+          return false;
+
+      return true;
+    }
+#endif
 
   }
 }
